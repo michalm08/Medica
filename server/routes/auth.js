9ade3c55152fbb05f7c1a491a60440341606e3d6
Michal Michalski <michalmichalski@MacBook-Air-Michal.local>
2021-02-24 10:22:38 +0100

    made firsts  routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 
 // @route GET api/auth
 // @desc Test
 // @access Public
-router.get('/', (req,res)=>res.send('User route'))
+router.get('/', (req,res)=>res.send('Auth route'))
 
 module.exports= router
--- server/server.js
+++ server/server.js
@@ -4,9 +4,16 @@ const connectDB = require('./db')
 
 const app = express();
 
+//Connect to DB
 connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
+
+//Defin Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/posts', require('./routes/posts'))
+app.use('/api/profile', require('./routes/profile'))
+app.use('/api/users', require('./routes/users'))
 
 const PORT = process.env.PORT || 5000;
 
