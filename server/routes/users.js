1b05f3a245a1bfd3fae1212ef3faef291030b687
Michal Michalski <michalmichalski@MacBook-Air-Michal.local>
2021-02-24 12:48:57 +0100

    encrypt password

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const User = require("../models/User.js");
+const bcrypt = require("bcryptjs");
 
 // @route POST api/users
 // @desc Register User
@@ -15,7 +16,7 @@ router.post(
     body("password", "password is to short").isLength({ min: 6 }),
   ],
   async (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
 
     //z dokumentacji express validator
     const errors = validationResult(req);
@@ -25,22 +26,31 @@ router.post(
 
     const { name, lastName, email, password } = req.body;
     try {
-      // if user exists
+      //if user exists
       let user = await User.findOne({ email });
       if (user) {
         return res.status(400).json({ msg: "user exists" });
       }
-      
-      //encrypt password
 
-      
-      //add user do database
+      //tworzeusera
       user = new User({ name, lastName, email, password });
       console.log(user);
+
+      //encrypt password dodaje to niego zakodowane haslo
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      console.log(user);
+
+      //add user do database
       await user.save();
 
-      //retunr jsonWebToken
-    } catch (error) {}
+      //return jsonwebtoken
+
+      
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
 
     res.send("User route");
   }
