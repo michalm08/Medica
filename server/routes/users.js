d7ae0e437f2c2a6857ee83fb74fb63874c24b7db
Michal Michalski <michalmichalski@MacBook-Air-Michal.local>
2021-02-24 11:35:06 +0100

    create validation to register user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-// @route GET api/users
+// @route POST api/users
 // @desc Test
 // @access Public
-router.get('/', (req,res)=>res.send('User route'))
+router.post(
+  "/",
+  [
+    body("name", "Name is required").notEmpty(),
+    body("lastName", "Name is required").notEmpty(),
+    body("email", "Include valid email").isEmail(),
+    body("password", "password is to short").isLength({ min: 6 }),
+    body("password2", "password2 is to short").isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    console.log(req.body);
 
-module.exports= router
+    //z dokumentacji express validator
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User route");
+  }
+);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
 const express = require("express");
-const connectDB = require('./db')
-
+const connectDB = require("./db");
 
 const app = express();
 
 //Connect to DB
 connectDB();
 
+//this allow us take data from req.body
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
 
-//Defin Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/posts', require('./routes/posts'))
-app.use('/api/profile', require('./routes/profile'))
-app.use('/api/users', require('./routes/users'))
+//Define Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/posts", require("./routes/posts"));
+app.use("/api/profile", require("./routes/profile"));
+app.use("/api/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server is working at ${PORT}`));
