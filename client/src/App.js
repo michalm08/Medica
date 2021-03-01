31fd0933f0611b380ecdb86fad4aa893629696d3
Michal Michalski <michalmichalski@MacBook-Air-Michal.local>
2021-03-01 13:04:19 +0100

    po 37 readux zainicjalowany wlasnie

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,26 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-          </Switch>
-        </section>
-
-        
-      </>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Switch>
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+            </Switch>
+          </section>
+        </>
+      </Router>
+    </Provider>
   );
 };
 
